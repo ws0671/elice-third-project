567b17b023e705571bf44dfe3e3e88efc37051aa
[4기-1반] 이예원 <panggun503@gmail.com>
2022-06-01 15:46:42 +0900

    feat: mongoDB 연결 후 back 실습

--- back/index.js
+++ back/index.js
@@ -1,9 +1,26 @@
-
+import "dotenv/config";
 import { app } from "./src/app";
 
-const PORT = process.env.SERVER_PORT || 5001;
+const PORT = process.env.SERVER_PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`port ${PORT} is running`);
+  console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
 });
 
+import mongoose from "mongoose";
+
+const DB_URL =
+  process.env.MONGODB_URL ||
+  "MongoDB 서버 주소가 설정되지 않았습니다.\n./db/index.ts 파일을 확인해 주세요.";
+
+mongoose.connect(DB_URL);
+const db = mongoose.connection;
+
+db.on("connected", () =>
+  console.log("정상적으로 MongoDB 서버에 연결되었습니다.  " + DB_URL)
+);
+db.on("error", (error) =>
+  console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
+);
+
+export { db };

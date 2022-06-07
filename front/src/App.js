787eb121b2e5f3ebf882e0929725bdd1bf01224b
[4기-1반]김애림 <pinkpig_21@naver.com>
2022-06-07 11:04:07 +0900

    front router 설정

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>front</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" exact element={<Home />} />
+                <Route path="/walk" exact element={<WalkPage />} />
+                <Route path="/hospital" exact element={<HospitalPage />} />
+                <Route path="/login" exact element={<LoginPage />} />
+                <Route path="/register" exact element={<RegisterPage />} />
+                <Route path="/board" exact element={<BoardPage />} />
+                <Route path="/ai" exact element={<AiPage />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;

11fd143f27574e847474f0f2bdf6aa5c99d295c8
[4기-1반]김애림 <pinkpig_21@naver.com>
2022-06-09 22:41:47 +0900

    style : board

--- front/src/components/common/board/Post.js
+++ front/src/components/common/board/Post.js
@@ -1,4 +1,6 @@
-import { Grid } from "@mui/material";
+import { Grid, Button, InputBase } from "@mui/material";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import IconButton from "@mui/material/IconButton";
 import { Container } from "@mui/system";
 import {
     Left,
@@ -8,6 +10,15 @@ import {
     UserName,
     UserDate,
     UserImg,
+    Content,
+    PostImg,
+    PostTag,
+    PostInfo,
+    Comment,
+    CommentName,
+    CommentEditor,
+    CommentInput,
+    CommentWrite,
 } from "./PostStyle";
 
 const Post = () => {
@@ -24,12 +35,93 @@ const Post = () => {
                 <Left>
                     <User>
                         <UserImg />
-                        <UserName />
-                        <UserDate />
+                        <UserName>Aerim</UserName>
+                        <UserDate>
+                            2022.06.09
+                            <Button
+                                sx={{
+                                    padding: "0",
+                                }}
+                            >
+                                수정
+                            </Button>
+                            <Button
+                                sx={{
+                                    padding: "0",
+                                }}
+                            >
+                                삭제
+                            </Button>
+                        </UserDate>
                     </User>
-                    <Title>제목</Title>
+                    <Title>[초보집사] 고양이 산책! 궁금해요! </Title>
+
+                    <PostImg>이미지</PostImg>
+                    <Content>고양이도 산책 할 수 있나요?</Content>
+                    <PostTag>
+                        <Button
+                            sx={{
+                                color: "#818479",
+                                border: "solid 1px",
+                                borderRadius: "20px",
+                                fontSize: "0.5rem",
+                                margin: "0 5px 0 0",
+                            }}
+                        >
+                            해시태그
+                        </Button>
+                    </PostTag>
                 </Left>
-                <Right>댓글쪽</Right>
+                <Right>
+                    <PostInfo>
+                        <IconButton>
+                            <FavoriteBorderIcon sx={{ margin: "0 5%" }} />
+                        </IconButton>
+                        1541
+                    </PostInfo>
+                    <Comment>
+                        <CommentName>닉네임</CommentName>
+                        가나다라마바사아자차카타파하
+                        <br />
+                        <Button
+                            size="small"
+                            sx={{
+                                padding: "0",
+                                width: "15px",
+                            }}
+                        >
+                            수정
+                        </Button>
+                        <Button
+                            size="small"
+                            sx={{
+                                padding: "0",
+                                color: "Red",
+                            }}
+                        >
+                            삭제
+                        </Button>
+                    </Comment>
+                    <Comment>
+                        <CommentName>닉네임</CommentName>안녕
+                    </Comment>
+
+                    <CommentWrite>
+                        <InputBase
+                            variant="standard"
+                            placeholder="댓글을 입력해 주세요."
+                        />
+                        <Button
+                            size="small"
+                            sx={{
+                                padding: "0",
+                                width: "15px",
+                            }}
+                        >
+                            입력
+                        </Button>
+                    </CommentWrite>
+                </Right>
             </Container>
         </>
     );
--- front/src/components/common/board/PostStyle.js
+++ front/src/components/common/board/PostStyle.js
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Grid, Input } from "@mui/material";
 import styled from "styled-components";
 
 const Left = styled(Grid)`
@@ -6,39 +6,119 @@ const Left = styled(Grid)`
     border: solid 1px #d9d9d9;
     border-radius: 7px;
     min-height: 600px;
-    padding: 3%;
+    padding: 1% 3%;
+    position: relative;
 `;
 
 const Right = styled(Grid)`
     width: 29%;
     border: solid 1px #d9d9d9;
+    border-radius: 7px;
+    padding: 1% 3%;
+    position: relative;
 `;
 const User = styled(Grid)`
     border-bottom: solid 1px #d9d9d9;
     display: flex;
     justify-content: space-between;
-    padding: 0 0 2% 0;
+    padding: 0 0 1% 0;
 `;
 
 const UserImg = styled(Grid)`
-    width: 50px;
-    height: 50px;
+    width: 42px;
+    height: 42px;
     border-radius: 100%;
     background-color: gray;
 `;
 const UserName = styled(Grid)`
-    width: 60%;
-    background-color: gray;
+    width: 40%;
+    color: gray;
+    padding: 10px 5px;
+    font-size: 17px;
 `;
 
 const UserDate = styled(Grid)`
-    width: 25%;
-    background-color: gray;
+    width: 50%;
+    color: gray;
+    font-size: 15px;
+    text-align: right;
+    padding: 10px 0;
 `;
 
 const Title = styled(Grid)`
-    font-size: 15px;
+    font-size: 18px;
     color: #464646;
+    margin: 3% 3% 0 3%;
+`;
+const PostImg = styled(Grid)`
+    background-color: gray;
+    margin: 2% 3%;
 `;
 
-export { Left, Right, Title, User, UserImg, UserName, UserDate };
+const Content = styled(Grid)`
+    font-size: 15px;
+    color: #464646;
+    margin: 2% 0;
+    margin: 2% 3%;
+`;
+
+const PostTag = styled(Grid)`
+    padding: 2% 0;
+    top: 90%;
+    position: absolute;
+`;
+
+const PostInfo = styled(Grid)`
+    border-bottom: solid 1px #d9d9d9;
+    height: 47px;
+    font-size: 14px;
+    color: gray;
+    margin: 1% 0;
+`;
+
+const Comment = styled(Grid)`
+    margin: 5% 0;
+    font-size: 14px;
+`;
+
+const CommentName = styled(Grid)`
+    font-size: 14px;
+    font-weight: bold;
+    color: #505050;
+`;
+
+const CommentWrite = styled(Grid)`
+    top: 90%;
+    position: absolute;
+    border-top: solid 1px #d9d9d9;
+    width: 80%;
+    padding: 3%;
+    display: flex;
+`;
+
+const CommentInput = styled(Input)`
+    width: 100%;
+    border: none;
+
+    &:hover {
+        border-bottom: none;
+    }
+`;
+
+export {
+    Left,
+    Right,
+    Title,
+    User,
+    UserImg,
+    UserName,
+    UserDate,
+    Content,
+    PostImg,
+    PostTag,
+    PostInfo,
+    Comment,
+    CommentName,
+    CommentWrite,
+    CommentInput,
+};

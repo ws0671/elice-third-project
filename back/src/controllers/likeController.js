16eac198a380f55caf6f074b62ca800cd2d8134d
[4기-1반] 이예원 <panggun503@gmail.com>
2022-06-28 14:05:18 +0900

    feat: like boards 수정 및 places 추가

--- back/src/controllers/likeController.js
+++ back/src/controllers/likeController.js
@@ -5,13 +5,8 @@ class likeController {
   // 좋아요 정보를 담을 json 생성 (회원가입 시 자동 생성)
   static createLikeInfo = async (req, res, next) => {
     try {
-      if (is.emptyObject(req.body)) {
-        throw new Error(
-          "요청 내용이 빈 객체입니다. headers의 Content-Type을 application/json으로 설정해주세요"
-        );
-      }
-
       const userId = req.body.userId;
+
       const newLike = await likeService.addLikeInfo({
         userId,
       });
@@ -22,68 +17,73 @@ class likeController {
     }
   };
 
-  // 해당 유저가 좋아요한 게시글의 id 배열 조회
-  static getLikedBoardIdArray = async (req, res, next) => {
+  // scope에 맞는 좋아요 정보 조회
+  static getLikeInfo = async (req, res, next) => {
     try {
       const userId = req.currentUserId;
-      const likedBoardIdArray = await likeService.findLikedBoardIdArray({
+      const scope = req.query.scope;
+
+      const likeInfo = await likeService.findLikeInfo({
         userId,
+        scope,
       });
 
-      res.status(200).json(likedBoardIdArray);
+      res.status(200).json(likeInfo);
     } catch (error) {
       next(error);
     }
   };
 
-  // 해당 유저가 좋아요한 장소 정보 조회
-  static getLikedPlaceInfo = async (req, res, next) => {
+  // 좋아요 상태 수정
+  static editLikedInfo = async (req, res, next) => {
     try {
       const userId = req.currentUserId;
-      const category = req.params.category;
+      const scope = req.query.scope;
 
-      const likedPlaceInfo = await likeService.findLikedPlaceInfo({
-        userId,
-        category,
-      });
+      // scope가 boards일 경우 게시글 좋아요 수정
+      if (scope === "boards") {
+        const boardId = req.body.boardId;
+        const toUpdate = { boardId };
 
-      res.status(200).json(likedPlaceInfo);
-    } catch (error) {
-      next(error);
-    }
-  };
+        const likedBoardIdArray = await likeService.setLikedBoardIdArray({
+          userId,
+          toUpdate,
+        });
 
-  // 게시글 좋아요 상태 수정
-  static editLikedBoardIdArray = async (req, res, next) => {
-    try {
-      const userId = req.currentUserId;
-      const boardId = req.body.boardId;
+        res.status(200).json(likedBoardIdArray);
+      }
+      // scope가 places일 경우 장소 좋아요 수정
+      else if (scope === "places") {
+        const {
+          id,
+          place_name,
+          phone,
+          x,
+          y,
+          address_name,
+          road_address_name,
+          category,
+          place_url,
+        } = req.body;
+        const toUpdate = {
+          id,
+          place_name,
+          phone,
+          x,
+          y,
+          address_name,
+          road_address_name,
+          category,
+          place_url,
+        };
 
-      const toUpdate = { boardId };
-      const likedBoardIdArray = await likeService.setLikedBoardIdArray({
-        userId,
-        toUpdate,
-      });
+        const likedPlaceArray = await likeService.setLikedPlaceArray({
+          userId,
+          toUpdate,
+        });
 
-      res.status(200).json(likedBoardIdArray);
-    } catch (error) {
-      next(error);
-    }
-  };
-
-  // 장소 좋아요 상태 수정
-  static editLikedPlaceArray = async (req, res, next) => {
-    try {
-      const userId = req.currentUserId;
-      const placeCategory = req.body.placeCategory;
-
-      const toUpdate = { placeCategory };
-      const likedBoardIdArray = await likeService.setLikedBoardIdArray({
-        userId,
-        toUpdate,
-      });
-
-      res.status(200).json(likedBoardIdArray);
+        res.status(200).json(likedPlaceArray);
+      }
     } catch (error) {
       next(error);
     }
--- back/src/db/schemas/like.js
+++ back/src/db/schemas/like.js
@@ -1,5 +1,42 @@
 import { Schema, model } from "mongoose";
 
+const PlaceSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  place_name: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  x: {
+    type: String,
+    required: true,
+  },
+  y: {
+    type: String,
+    required: true,
+  },
+  address_name: {
+    type: String,
+    required: true,
+  },
+  road_address_name: {
+    type: String,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  place_url: {
+    type: String,
+    required: true,
+  },
+});
+
 const LikeSchema = new Schema({
   userId: {
     type: String,
@@ -9,7 +46,7 @@ const LikeSchema = new Schema({
     type: [String],
   },
   placeArray: {
-    type: Schema.Types.Mixed,
+    type: [PlaceSchema],
   },
 });
 

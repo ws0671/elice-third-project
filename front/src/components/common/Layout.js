9e69564d13ac5816ee622b03eeddb93212117303
[4기-1반]김애림 <pinkpig_21@naver.com>
2022-06-29 17:52:35 +0900

    feat : mobile header menu 구현 완료

--- front/src/components/common/Layout.js
+++ front/src/components/common/Layout.js
@@ -2,7 +2,7 @@ import { Container } from "@mui/material";
 import styled from "styled-components";
 
 const Layout = styled(Container)`
-  padding-top: 160px;
+    padding-top: 110px;
 `;
 
 export default Layout;
